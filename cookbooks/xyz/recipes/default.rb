7256899e40e97811fcbf8578a2343be221386ffc
root <root@localhosts>
2016-09-20 23:43:37 +0530

    first

---
+++ cookbooks/xyz/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: xyz
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+  template "/etc/abc" do
+       source "abc.erb"
+  end
+
+
+
+
